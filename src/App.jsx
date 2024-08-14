62a29f5a82ba1a7ea2aa5a2c3761638d516f28bc
= <=>
2024-08-14 20:57:12 -0300

    parcial finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,86 @@
 import './App.css';
+import React, { useState } from "react";
 import Card from './Components/Card';
+
 
 function App() {
 
+  //const [name, setName] = useState("");
+ // const [title, setTitle] = useState("");
+
+  const [user, setUser] = useState({
+    name: "",
+    title: "",
+  });
+
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChangeName = (event) => {
+    console.log(user.name)
+    setUser({ ...user, name: event.target.value });
+  };
+  const handleChangeTitle = (event) => {
+    console.log(user.title)
+    setUser({ ...user, title: event.target.value });
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log(user.name.includes(" "));
+    console.log(user.name.trim().length > 3);
+    console.log(user)
+    if (
+      user.name.length > 3 &&
+      user.title.length > 6
+    ) {
+      setShow(false);
+      setError(true);
+    } else {
+      setShow(false);
+      setError(true);
+    }
+  };
+
+  const reset = () => {
+    setName("");
+    setTitle("");
+
+  };
 
   return (
+
+    <>
+    {show ? ( 
     <>
     <div className='App'>
-      <form></form>
-      <Card/>
+    <form onSubmit={handleSubmit}>
+        <label >Nombre:</label>
+        <input 
+          type="text"
+          value={user.name}
+          onChange={handleChangeName}    
+        />
+        <label>Titulo Libro:</label>
+        <input
+          type="text"
+          value={user.title}
+          onChange={handleChangeTitle}   
+        />
+        <button>Enviar</button>
+    </form>
+    <button 
+        onClick={reset}>Resetear formulario
+    </button>
     </div>
+    <Card user={ user } />
+    </>
+    ):(
+    <h3 style={{ color: "red" }}>Por favor introduzca nuevamente la información</h3>
+    )}
+   
     </>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Card = () => {
-  return <><div>Hola, soy una tarjeta</div></>
+const Card = ( user ) => {
+
+return <>
+
+    <h4>Muchas gracias, {user.name}!</h4>
+    <h4>
+        El libro que elegiste es {" "}
+        {user.title}
+    </h4>
+ </>
+
 };
 
 export default Card;
